61099556e3c1e6822441d774fa098d9d3a47b810
ankit <ankitkarapurkar426@gmail.com>
2023-04-16 16:02:03 +0530

    contacts n about page added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const port = 3000
 const { engine } = require('express-handlebars')
 const layout_data = require('./public/assets/json/layout_data.json');
-const page_data = require('./public/assets/json/index_data.json');
+
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -14,22 +14,32 @@ app.use(express.static('public'));
 
 // route: Get '/'
 app.get('/', (req, res) => {
+  const page_data = require('./public/assets/json/index_data.json');
   res.render('users/index',page_data);
 })
 
 // route: Get '/services'
 app.get('/services', (req, res) => {
+  const page_data = require('./public/assets/json/index_data.json');
   res.render('users/services');
 })
 
 // route: Get '/projects'
 app.get('/projects', (req, res) => {
+  const page_data = require('./public/assets/json/index_data.json');
   res.render('users/projects');
 })
 
 // route: Get '/contact'
 app.get('/contact', (req, res) => {
-  res.render('users/contact');
+  const page_data = require('./public/assets/json/contact_data.json');
+  res.render('users/contact',page_data);
+})
+
+// route: Get '/about'
+app.get('/about', (req, res) => {
+  const page_data = require('./public/assets/json/about_data.json');
+  res.render('users/about',page_data);
 })
 
 app.listen(port, () => {
